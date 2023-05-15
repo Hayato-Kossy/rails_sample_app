8d1c5f86cc6831c8a31cc849af7e7f1b0464e905
Hayato Koshi <kshyt2501@icloud.com>
2023-05-15 23:24:22 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem "faker",           "2.21.0"
 gem "will_paginate",           "3.3.1"
 gem "bootstrap-will_paginate", "1.0.0"
+gem "active_storage_validations", "0.9.8"
 # Use sqlite3 as the database for Active Record
 # gem "sqlite3", "~> 1.4"
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
-
   def new
     @user = User.new
   end
@@ -48,28 +48,18 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
     end
 
-        # beforeフィルタ
+    # beforeフィルター
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-
-        # 正しいユーザーかどうか確認
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)    
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 
-    # 管理者かどうか確認
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
